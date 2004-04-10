29ad17831cde3c9277173573d6a2acb3b71b4a3e
Mike Prosser <mikeprosser@users.sourceforge.net>
2004-04-10 10:09:56 +0000

    Changed to Sprites, found too-thin bug in Ogre::Billboards

---
+++ OgrianPhysicalEntity.cpp
@@ -0,0 +1,91 @@
+#include "OgrianPhysicalEntity.h"
+namespace Ogrian
+{
+
+unsigned long PhysicalEntity::msNextGeneratedNameExt = 1;
+
+PhysicalEntity::PhysicalEntity(String material, Real x, Real y, Real z)
+{
+    // Generate a name
+    static char name[64];
+	sprintf(name, "PE_%lu", msNextGeneratedNameExt++);
+
+	SceneManager* sceneMgr = Renderer::getSingleton().getSceneManager();
+
+	bbset = sceneMgr->createBillboardSet(name,1);
+	billboard = bbset->createBillboard(x, y, z);
+	bbset->setMaterialName(material);
+
+	bbset->setBillboardType(BBT_ORIENTED_COMMON);
+	bbset->setCommonDirection(Vector3::UNIT_Y);
+
+	sceneMgr->getRootSceneNode()->attachObject(bbset);
+
+	setPosition(x, y, z);
+}
+
+void PhysicalEntity::setVelocity(Vector3 vel)
+{
+	setVelocity(vel.x, vel.y, vel.z);
+}
+void PhysicalEntity::setPosition(Vector3 pos)
+{
+	setPosition(pos.x, pos.y, pos.z);
+}
+
+void PhysicalEntity::setPosition(Real x, Real y, Real z)
+{
+	pos.x = x;
+	pos.y = y;
+	pos.z = z;
+
+	billboard->setPosition(x, y, z);
+}
+
+
+void PhysicalEntity::setVelocity(Real x, Real y, Real z)
+{
+	vel.x = x;
+	vel.y = y;
+	vel.z = z;
+}
+
+void PhysicalEntity::setScale(Real scale)
+{
+	billboard->setDimensions(scale,scale);
+
+	radius = scale/2;
+}
+
+void PhysicalEntity::move(Real time)
+{
+	setPosition(
+		pos.x + vel.x * time,
+		pos.y + vel.y * time,
+		pos.z + vel.z * time);
+}
+
+Real PhysicalEntity::distance(PhysicalEntity* e)
+{
+	return sqrt((pos.x - e->pos.x)*(pos.x - e->pos.x) 
+			    + (pos.z - e->pos.z)*(pos.z - e->pos.z));
+}
+
+Real PhysicalEntity::getRadius()
+{
+	return radius;
+}
+
+void PhysicalEntity::collided(PhysicalEntity* e)
+{
+	// override this for interesting behaviors
+	
+}
+
+// they are ordered by x location
+bool PhysicalEntity::operator<(PhysicalEntity* other)
+{
+	return (pos.x < other->pos.x);
+}
+
+}
--- OgrianPhysicalEntity.h
+++ OgrianPhysicalEntity.h
@@ -16,93 +16,28 @@ class PhysicalEntity
 public:
 	Vector3 pos;
 
-	PhysicalEntity(String material, Real x=0, Real y=0, Real z=0)
-	{
-		LogManager::getSingleton().logMessage("adding physical entity");
+	PhysicalEntity(String material, Real x=0, Real y=0, Real z=0);
 
-        // Generate a name
-        static char name[64];
-		sprintf(name, "Unnamed_%lu", msNextGeneratedNameExt++);
+	virtual void setVelocity(Vector3 vel);
+	virtual void setPosition(Vector3 pos);
 
-		SceneManager* sceneMgr = Renderer::getSingleton().getSceneManager();
-
-		bbset = sceneMgr->createBillboardSet(name,1);
-		billboard = bbset->createBillboard(x, y, z);
-		bbset->setMaterialName(material);
-
-		//bbset->setBillboardType(BBT_ORIENTED_COMMON);
-		//bbset->setCommonDirection(Vector3::UNIT_Y);
-
-		sceneMgr->getRootSceneNode()->attachObject(bbset);
-
-		setPosition(x, y, z);
-
-		LogManager::getSingleton().logMessage("added physical entity");
-	}
-
-	virtual void setVelocity(Vector3 vel)
-	{
-		setVelocity(vel.x, vel.y, vel.z);
-	}
-	virtual void setPosition(Vector3 pos)
-	{
-		setPosition(pos.x, pos.y, pos.z);
-	}
-
-	virtual void setPosition(Real x, Real y, Real z)
-	{
-		pos.x = x;
-		pos.y = y;
-		pos.z = z;
-
-		billboard->setPosition(x, y, z);
-	}
+	virtual void setPosition(Real x, Real y, Real z);
 
 
-	virtual void setVelocity(Real x, Real y, Real z)
-	{
-		vel.x = x;
-		vel.y = y;
-		vel.z = z;
-	}
+	virtual void setVelocity(Real x, Real y, Real z);
 
-	virtual void setScale(Real scale)
-	{
-		billboard->setDimensions(scale,scale);
+	virtual void setScale(Real scale);
 
-		radius = scale/2;
-	}
+	virtual void move(Real time);
 
-	virtual void move(Real time)
-	{
-		setPosition(
-			pos.x + vel.x * time,
-			pos.y + vel.y * time,
-			pos.z + vel.z * time);
-	}
+	virtual Real distance(PhysicalEntity* e);
 
-	virtual Real distance(PhysicalEntity* e)
-	{
-		return sqrt((pos.x - e->pos.x)*(pos.x - e->pos.x) 
-			      + (pos.z - e->pos.z)*(pos.z - e->pos.z));
-	}
+	virtual Real getRadius();
 
-	virtual Real getRadius()
-	{
-		return radius;
-	}
-
-	virtual void collided(PhysicalEntity* e)
-	{
-		// override this for interesting behaviors
-		
-	}
+	virtual void collided(PhysicalEntity* e);
 
 	// they are ordered by x location
-	bool operator<(PhysicalEntity* other)
-	{
-		return (pos.x < other->pos.x);
-	}
+	bool operator<(PhysicalEntity* other);
 
 	
 private:
