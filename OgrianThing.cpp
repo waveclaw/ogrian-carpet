062a8ce3f4b78500b845a2b54ad3c78923cc92ca
Mike Prosser <mikeprosser@users.sourceforge.net>
2005-01-09 07:58:12 +0000

    made cranes flap on clients

--- OgrianThing.cpp
+++ OgrianThing.cpp
@@ -337,6 +337,18 @@ void Thing::move(Real time)
 			mLastThinkTime = now + period * Math::RangeRandom(0,.1);
 		}
 	}
+	else
+	{
+		// see if it's time to think yet
+		unsigned long now = Time::getSingleton().getTime();
+		unsigned long period = unsigned long(CONR("THING_THINK_PERIOD") * 1000);
+		if (mLastThinkTime + period < now)
+		{
+			clientThink();
+
+			mLastThinkTime = now + period * Math::RangeRandom(0,.1);
+		}
+	}
 }
 
 //----------------------------------------------------------------------------
