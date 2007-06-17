72dc541a0371e21442fc4678375436108e77dda5
Jeremiah D. Powell <waveclaw@users.sourceforge.net>
2007-06-17 20:50:20 +0000

    added Unit tests and stubs for isolation.

---
+++ Tests/AudioTest.cpp
@@ -0,0 +1,154 @@
+/*****************************************************************************
+	Copyright 2004 Mike Prosser
+
+    This file is part of Ogrian Carpet.
+
+    Ogrian Carpet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Ogrian Carpet is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ogrian Carpet; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*****************************************************************************/
+
+/**
+ * \note Usage
+ *  In the src/ directory:
+ * 1. copy or link this file and Patches/stub.cpp to the src directory.
+ *    and make the header changes in ../include/OgrianAudio.h or -DUSE_STUB_H in 
+ *    the Makefile
+ * 2. build it's pre-req with
+ * \code 
+  	make OgrianAudio.o audioTest.o stub.o
+ * \endcode
+ * 3. link with
+  * \code 
+  	/bin/sh ../libtool --tag=CXX --mode=link g++ -DUSE_STUB_H -DOGRE_GUI_gtk \
+   -DOGRE_CONFIG_LITTLE_ENDIAN -I/usr/local/include \
+   -I/usr/local/include/OGRE   -I/usr/local/include/RakNet \
+   -I/usr/local/include   -I/usr/local/include/fmodex/   -I/usr/include/CEGUI \
+   -Wall -Wimplicit -Wunused -g -pg -g -O2 -L/usr/local/lib64 -lOgreMain \
+   -L/usr/local/lib64 -lRakNet -lpthread   -lfmodex64   -lCEGUIBase  \
+   -L/usr/local/lib64 -lOIS    -o test OgrianAudio.o audioTest.o stub.o
+ * \endcode   
+ * 4. run the test with
+ * \code
+   test ../Media/sounds/hum1.wav ../Media/music/Bulerias.ogg
+ * \endcode
+ */
+
+/**
+ * \file AudioTest.cpp
+ * \author Jeremiah Powell<waveclaw@hot_nospam_mail.com
+ * \brief Unit test for the Ogrian Audio system.
+ * Unit testing the Audio system can use either the existing system or
+ * the stub system.  To use the stub system, define USE_STUB_H when compling. 
+ */
+#include "OgrianAudio.h"
+#include <iostream>
+
+using namespace std;
+using namespace Ogrian;
+
+#ifdef WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT unused)
+
+#else // #ifdef WIN32
+
+int main(int argc, char *argv[])
+
+#endif // #ifdef WIN32
+{
+
+	Audio &testSoundPlayer = Audio::getSingleton();
+	FMOD::Channel* channel;
+	Vector3 position = Vector3(0.0, 0.0, 0.0);
+	Real time = 1.0;
+	Real volume = 5.0;
+	
+	if (argc != 3) {
+		cout << "I want a sound followed by a song." << endl;
+		return 1;
+	};
+	
+	String soundfile = argv[1];
+	String songfile = argv[2];
+	int id;
+	
+	id = testSoundPlayer.loadSound(soundfile); // islong = false, loop = false
+	if (-1 == id) {
+		cout << "Unable to load Sound" << soundfile << endl;
+		return 1;
+	};
+
+	testSoundPlayer.start(); // volume up
+	channel = testSoundPlayer.playSound(id, position);
+	cout << "Testing sound: ok" << endl;
+	testSoundPlayer.stop();	//volume off
+
+	if (testSoundPlayer.isLong(id)) {
+			cout << "A short sound reported false longness" << endl;		
+			return 1;
+	}
+	
+	sleep(1);
+	testSoundPlayer.start(); // volume up
+	position = Vector3(0.1, 0.1, 0.1);
+	testSoundPlayer.setSoundPosition(channel, position);
+	channel = testSoundPlayer.playSound(id, position);
+	cout << "Testing Moved sound: ok" <<  endl;
+
+	position = Vector3(0, 0, 0);
+	testSoundPlayer.setSoundPosition(channel, position);
+	testSoundPlayer.stop();	//volume off
+	
+	testSoundPlayer.playSong(songfile, volume);
+	testSoundPlayer.start(); // volume up
+	cout << "Testing song:";cout.flush();
+	for (int i=1;i<5;i++) {
+		testSoundPlayer.frame(time); // play for a while
+		sleep(1);
+		cout << ".";cout.flush();
+	};
+	cout << "ok" << endl;
+	testSoundPlayer.stop();	//volume off
+	
+
+	testSoundPlayer.stopSong();
+	testSoundPlayer.start(); // volume up
+	cout << "Testing stopped song:";cout.flush();
+	for (int i=1;i<5;i++) {
+		testSoundPlayer.frame(time); // play for a while
+		sleep(1);
+		cout << ".";cout.flush();
+	};
+	cout << "ok" << endl;
+	testSoundPlayer.stop();	//volume off
+
+	testSoundPlayer.playSong(songfile, volume);
+	testSoundPlayer.start(); // volume up
+	cout << "Testing song and sound:";cout.flush();
+	for (int i=1;i<5;i++) {
+		testSoundPlayer.frame(time); // play for a while
+		sleep(1);
+		if (i % 2)	channel = testSoundPlayer.playSound(id, position);
+		cout << ".";cout.flush();
+	};
+	cout << "ok" << endl;
+	testSoundPlayer.stop();	//volume off
+
+	testSoundPlayer.stopSound(channel);	
+
+    return 0;
+}
---
+++ Tests/stub.cpp
@@ -0,0 +1,69 @@
+//this file is used to bypass files that prevent compling due to out of date dependancies.
+//Ogre::Vector3 defined in <Ogre.h>
+
+#include "../Tests/stub.hpp"
+
+template<> Ogrian::Renderer* Singleton< Ogrian::Renderer >::ms_Singleton = 0;
+template<> Ogrian::Game* Singleton< Ogrian::Game >::ms_Singleton = 0;
+template<> Ogrian::Hud* Singleton< Ogrian::Hud >::ms_Singleton = 0;
+template<> Ogrian::SpellManager* Singleton< Ogrian::SpellManager >::ms_Singleton = 0;
+template<> Ogrian::Menu* Singleton< Ogrian::Menu >::ms_Singleton = 0;
+template<> Ogrian::Physics* Singleton< Ogrian::Physics >::ms_Singleton = 0;
+
+
+namespace Ogrian {
+
+Physics& Physics::getSingleton(void) { if (!ms_Singleton) ms_Singleton = new Physics(); return Singleton<Physics>::getSingleton(); }
+Physics::Physics() {}
+Thing *Physics::getThing(int id) { return new Thing();}
+int Physics::getTeam(int team) { return 0;}
+void Physics::addThing(WizardThing*&) {}
+void Physics::syncClient(void *) {}
+
+Renderer& Renderer::getSingleton(void) { if (!ms_Singleton) ms_Singleton = new Renderer(); return Singleton<Renderer>::getSingleton(); }
+Renderer::Renderer() {}
+CameraThing *Renderer::getCameraThing() { return new CameraThing();};
+String Renderer::getMapName(void) {return "foo";}
+void Renderer::loadMap(String map) {}
+
+Game& Game::getSingleton(void) { if (!ms_Singleton) ms_Singleton = new Game(); return Singleton<Game>::getSingleton(); }
+Game::Game() {}
+void victory(void) {};
+Vector3 getStartPos(void) { return Vector3(0.0f, 0.0f, 0.0f);}
+
+Menu& Menu::getSingleton(void) { if (!ms_Singleton) ms_Singleton = new Menu(); return Singleton<Menu>::getSingleton(); }
+Menu::Menu() {}
+String Menu::getChosenName(void) { return String(" ");};
+int Menu::getChosenServer(void) {return 0;};
+int Menu::getChosenCastleSkin(void) { return 0;}
+void Menu::hide(bool flag) {}
+void Menu::clearLoadingScreen(void) {}
+void show(bool) {}
+void button_disconnect(void) {}
+
+WizardThing::WizardThing(bool visible) {};
+
+Hud& Hud::getSingleton(void) { if (!ms_Singleton) ms_Singleton = new Hud(); return Singleton<Hud>::getSingleton(); }
+Hud::Hud() {}
+
+SpellManager& SpellManager::getSingleton(void) { if (!ms_Singleton) ms_Singleton = new SpellManager(); return Singleton<SpellManager>::getSingleton(); }
+SpellManager::SpellManager() {}
+
+Thing* Thing::getThing(int id) {return new Thing();}
+Thing::Thing() {}
+Vector3 Thing::getDirection(void) { return Vector3(0.0f, 0.0f, 0.0f);}
+Vector3 Thing::getVelocity(void) { return Vector3(0.0f, 0.0f, 0.0f);}
+Vector3 Thing::getPosition(void) { return Vector3(0.0f, 0.0f, 0.0f);}
+int Thing::getUID(void) { return 0;}
+int Thing::getTeamNum(void) { return 0;}
+void Thing::setPosition(Vector3) {}
+int Thing::setMessage(void *message) {return 0;}
+void Thing::_setUID(int uid) {}	 	
+void Thing::playSound(int sound) {}
+void Thing::addHealth(int hp) {}
+void Thing::setHealth(int hp) {}
+void Thing::die(void) {}
+void Thing::makeGhost(void) {}
+void Thing::handleMessage(String, Vector3, int) {}
+
+} // end Ogrian namespace
